c1cec3ff80f10bc2c83fb79e7966177f0bcf39e4
priscile <tatchumleone@gmail.com>
2020-12-09 17:54:08 +0100

    projet init

---
+++ index.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body style="background-color: #ddd">
+	<h1 style="padding-top:20%; padding-bottom:20%; text-align: center">bonjour</h1>
+</body>
+</html>
