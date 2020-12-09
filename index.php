fe36cf081f853b4a2382e9934ea764ec8de70efd
priscile <tatchumleone@gmail.com>
2020-12-09 18:08:16 +0100

    modif la couleur du body

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
 	<title></title>
 </head>
-<body style="background-color: #ddd">
-	<h1 style="padding-top:20%; padding-bottom:20%; text-align: center">bonjour</h1>
+<body style="background-color: green">
+	<h1 style="padding-top:20%; padding-bottom:20%; text-align: center; color">bonjour</h1>
 </body>
 </html>
