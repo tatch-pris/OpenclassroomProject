6bfa158ba35e2f60ac16aec5e0ce6f9fd2d42f39
priscile <tatchumleone@gmail.com>
2020-12-10 17:15:47 +0100

    modif de la couleur de fond

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
 	<title></title>
 </head>
-<body style="background-color: green">
+<body style="background-color: #bdc3c7">
 	<h1 style="padding-top:20%; padding-bottom:20%; text-align: center; color">bonjour</h1>
 </body>
 </html>
